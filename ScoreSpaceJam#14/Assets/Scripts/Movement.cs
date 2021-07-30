41d8b0761dacf3885361518245a4aa360f57786f
Diamondsword0 <diamondsword19@gmx.com>
2021-07-30 12:49:48 -0500

    sadmklasdjn'adf

--- ScoreSpaceJam#14/Assets/Scripts/Movement.cs
+++ ScoreSpaceJam#14/Assets/Scripts/Movement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float MovementSpeed;
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
     private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     
@@ -15,6 +18,11 @@ public class Movement : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
+
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) 
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
 
