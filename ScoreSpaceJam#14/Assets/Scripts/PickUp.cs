eae2578b557129218707d2d2a1442ec904d16233
ObsoleteBear <pauldavidreaves@gmail.com>
2021-07-31 16:36:17 -0500

    it doesnt work lol

--- ScoreSpaceJam#14/Assets/Scripts/PickUp.cs
+++ ScoreSpaceJam#14/Assets/Scripts/PickUp.cs
@@ -6,14 +6,17 @@ public class PickUp : MonoBehaviour
     public Transform grabDetect;
     public Transform boxholder;
     public float rayDist;
+    public float DistToPlayer;
 
     public GameObject Player;
     public Vector3 localpos;
     public float localrot;
     public bool connected = false;
+    public Rigidbody2D rb;
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
@@ -40,14 +43,35 @@ public class PickUp : MonoBehaviour
         //}
     }
 
+    public void FixedUpdate()
+    {
+        if (connected == true)
+        {
+
+            //gameObject.transform.localPosition = localpos;
+
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Player" && connected == false)
         {
-            gameObject.transform.parent = Player.transform;
-            localpos = gameObject.transform.localPosition;
+            //gameObject.transform.parent = Player.transform;
+            //localpos = gameObject.transform.localPosition;
             //localrot = gameObject.transform.localRotation.z;
-            connected = true;
+            //connected = true;
+            //gameObject.layer = 6;
+            // creates joint
+            DistanceJoint2D joint = gameObject.AddComponent<DistanceJoint2D>();
+            // sets joint position to point of contact
+            joint.anchor = collision.contacts[0].point;
+            // conects the joint to the other object
+            joint.connectedBody = collision.transform.GetComponent<Rigidbody2D>();
+            // Stops objects from continuing to collide and creating more joints
+            joint.enableCollision = false;
+            joint.autoConfigureDistance = false;
+            joint.distance = Vector2.Distance(gameObject.transform.position, collision.transform.position);
             gameObject.layer = 6;
         }
     }
