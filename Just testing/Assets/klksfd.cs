a10df577528246037c6ec8a0676de769282f53d7
Diamondsword0 <diamondsword19@gmx.com>
2021-08-11 14:08:09 -0500

    ;

--- Just testing/Assets/klksfd.cs
+++ Just testing/Assets/klksfd.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class klksfd : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public float jumpForce = 20f;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -18,5 +19,16 @@ public class klksfd : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) {
             rb.AddForce(new Vector2 (-300f, 0f) * Time.deltaTime);
         }
+
+        if(Input.GetButtonDown("Jump")) {
+            Jump();
+        }
+        
+
+    }
+    void Jump() {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
     }
 }
